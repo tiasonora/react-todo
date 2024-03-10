a0af8666d24a9e16c019227343c22da1221d94d8
Tia Jha <tia_jha@hotmail.com>
2024-03-10 03:08:28 -0400

    lists in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import * as React from 'react';
+
+const todoList = [
+  {
+    id:"1",
+    title:"Read assigned pages"
+  },
+  {
+    id:"2",
+    title:"Complete assignment"
+  },
+  {
+    id:"3",
+    title:"Submit assignment"
+  },
+];
+
 function App() {
 return (
 <div>
   <h1>Todo List</h1>
-  <ul></ul>
+
+  <ul>
+    {todoList.map(function(item){
+    return (
+      <li key={item.id}>
+        <span>
+        {item.title}
+        </span>
+      </li>
+    );  
+})}
+  </ul>
+
 </div>
 );
 }
